688da4c8aa6f25efa496f529e43c33087eb18bb8
AponeliosS <alperentarekallinone@gmail.com>
2026-07-24 13:12:48 +0300

    Delete main.lua

--- main.lua
+++
@@ -1,7 +0,0 @@
--- GitHub linklerinizden kodları canlı olarak indirip hafızaya yükler
--- (NOT: Buradaki linkleri kendi GitHub linklerinizle değiştireceksiniz)
-local fonksiyonDeposu = loadstring(game:HttpGet("https://githubusercontent.com"))()
-local menuDeposu = loadstring(game:HttpGet("https://githubusercontent.com"))()
-
--- Menüyü oluştur ve içine eylemi enjekte et!
-menuDeposu.Olustur(fonksiyonDeposu.otomatikTikla)
